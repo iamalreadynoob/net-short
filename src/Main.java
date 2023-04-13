07ab9b217b495250a26ccf15ac6ccd89c3902abd
iamalreadynoob <22120205381@ismu.edu.tr>
2023-04-13 15:07:06 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
-        String answer;
-
-        do
-        {
-            System.out.println("What do you want to do?");
-            new Command(new Scanner(System.in).nextLine(), new Parse());
-
-
-            System.out.println("ok? [y/n]");
-            answer = new Scanner(System.in).nextLine();
-            if (answer.charAt(0) != 'n') answer = "y";
-        }
-        while (answer.charAt(0) == 'n');
-
+        System.out.println("What do you want to do?");
+        new Command(new Scanner(System.in).nextLine(), new Parse());
     }
 }
